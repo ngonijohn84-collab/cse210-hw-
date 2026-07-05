0bde81e28d5a8398c31fbbbc0d7a7f6602c8488a
ngonijohn84 <ngonijohn84@gmail.com>
2026-07-05 20:43:25 +0300

    Complete Exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        int number;
+
+        do
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+
+        } while (number != 0);
+
+        int sum = 0;
+
+        foreach (int item in numbers)
+        {
+            sum += item;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
     }
 }
